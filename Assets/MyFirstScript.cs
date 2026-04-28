c4ec42943d86ae5608f599c52313b8c5f80ab355
raenyssa <raenz.yssa@gmail.com>
2026-04-28 11:15:00 +0800

    Week 2 activity

--- Assets/MyFirstScript.cs
+++ Assets/MyFirstScript.cs
@@ -1,16 +1,40 @@
+using System.Threading.Tasks;
 using UnityEngine;
+using UnityEngine.Scripting.APIUpdating;
 
 public class MyFirstScript : MonoBehaviour
 {
+
+    float move = 0.01f;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        print("Hello World");
+        print(transform.rotation);
     }
 
     // Update is called once per frame
     void Update()
     {
+
+        // Task 1
+        transform.position += new Vector3(move,0f,0f);
+
+        // Task 2
+        if (transform.position.x > 5f) move = -0.01f;
+        if (transform.position.x < -5f) move = 0.01f;
+
+        // Task 3
+        transform.Rotate(0, move, 0);
+
+        // Make the object grow larger by 0.1% every frame
+        // transform.localScale = transform.localScale * 1.001f;
+        
+
+        // How to declare a new variable
+        // var x = 1; This is an integer variable
+        // var v2 = new Vector2(0, 0);
+        // var v3 = new Vector3(0, 0, 0);
+        // var v4 = new Vector4(0, 0, 0, 0);
         
     }
 }
